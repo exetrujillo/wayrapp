091e1ab0111cf3085e046305a659ea83a068dc02
exetrujillo <exequiel.trujillo@ug.uchile.cl>
2025-07-27 14:39:12 -0400

    update docs x17

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 // Vercel serverless function handler
+// Initialize module aliases for @/ path resolution
+require('module-alias/register');
+
 const { default: app } = require('../dist/app.js');
 
 module.exports = app;
